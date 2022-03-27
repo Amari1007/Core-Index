5fedf5262d2c9345c3a9bd5f922243f950a1d7cd
Chaupi Amari Ghambi <amarighambi@gmail.com>
2022-03-28 00:34:20 +0200

    Update clubview.php

--- clubview.php
+++ clubview.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-require_once("include/coreDB.php");
+require_once("include/coreDB.php"); //import database connector
 
     //check if post or get method was used
             
